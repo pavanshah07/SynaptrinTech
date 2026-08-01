ee02b10235199a915c7c2876d940ae6f4b9a2a2f
Pavan shah <shahpavan46@gmail.com>
2026-08-02 01:23:20 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 
-{/* LIVE TEST */ }
-<div className="bg-red-600 text-white text-center py-4 text-2xl font-bold">
-  SYNAPTRINTECH LIVE UPDATE TEST
-</div>
+
 /**
  * @license
  * SPDX-License-Identifier: Apache-2.0
